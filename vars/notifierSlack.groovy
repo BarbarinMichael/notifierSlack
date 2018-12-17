ffc78f2369b1252db5531aadc1b5ff0a645edbf1
Michael Barbarin <39796794+BarbarinMichael@users.noreply.github.com>
2018-12-17 17:05:00 +0100

    Update notifierSlack.groovy

--- vars/notifierSlack.groovy
+++ vars/notifierSlack.groovy
@@ -1,25 +1,23 @@
 def call(String buildStatus = 'STARTED') {
 
-  // build status of null means successful
-  buildStatus = buildStatus ?: 'SUCCESSFUL'
+    // Default values
+    def summary = "${buildStatus}: ${env.JOB_NAME}"
 
-  // Default values
-  def summary = "${env.JOB_NAME}\n${env.BUILD_URL}"
+    // Override default values based on build status
+    if (buildStatus == 'SUCCESSFUL') {
+        //color = 'GREEN'
+        colorCode = '#0B6623'
+        summary += "\n${env.BUILD_URL}"
+    } else if (buildStatus == 'STARTED') {
+        //color = 'BLUE'
+        colorCode = '#0F52BA'
+    } else {
+        //color = 'RED'
+        colorCode = '#9E1A1A'
+        summary += "\n${env.BUILD_URL}"
+    }
 
-  // Override default values based on build status
-  if (buildStatus == 'SUCCESSFUL') {
-    //color = 'GREEN'
-    colorCode = '#0B6623'
-  } else if (buildStatus == 'STARTED') {
-    //color = 'BLUE'
-    colorCode = '#0F52BA'
-    summary = "${buildStatus}: ${env.JOB_NAME}"
-  } else {
-    //color = 'RED'
-    colorCode = '#9E1A1A'
-  }
-
-  // Send notifications
-  slackSend (color: colorCode, message: summary)
+    // Send notifications
+    slackSend(color: colorCode, message: summary)
 
 }
